5d06812f6441cf12eeb4d908398ec5ba581c5215
SosoZen <108021943+SosoZen@users.noreply.github.com>
2022-06-23 15:19:04 +0200

    Add files via upload

---
+++ labyrinthe/prototype.h
@@ -0,0 +1,9 @@
+
+
+int menu();
+int rejouer();
+int path_1();
+int path_2();
+int path_3();
+int is_valid(int x, int y, int hauteur, int largeur, char grille[hauteur][largeur]);
+int is_finish(int x, int y, int hauteur, int largeur, char grille[hauteur][largeur]);
