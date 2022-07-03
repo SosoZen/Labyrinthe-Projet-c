6203786a2f3ab066a23101e4a49a653088abb9ac
SosoZen <108021943+SosoZen@users.noreply.github.com>
2022-07-03 15:13:16 +0200

    Update replay.h

--- labyrinthe/replay.h
+++ labyrinthe/replay.h
@@ -3,6 +3,7 @@
 int rejouer(){
     system("clear");
     char choix;
+    printf("vous avez fini en %d pas",compteur);
     printf("Play again ? : \nY : Yes \nN : No \n");
     scanf("%s", &choix);
     if(choix== 'Y' || choix== 'y'){
